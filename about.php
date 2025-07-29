6f6bb602fc2b1a8f22bcc6176b1742a22daa7995
YouCARE1 <queen@example.com>
2025-07-29 13:29:02 +0500

    add about file

---
+++ about.php
@@ -0,0 +1,257 @@
+<?php include "header.php" ?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Medic | Medical HTML Template</title>
+
+  
+  <!-- mobile responsive meta -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  
+  <!-- Slick Carousel -->
+  <link rel="stylesheet" href="plugins/slick/slick.css">
+  <link rel="stylesheet" href="plugins/slick/slick-theme.css">
+  <!-- FancyBox -->
+  <link rel="stylesheet" href="plugins/fancybox/jquery.fancybox.min.css">
+  
+  <!-- Stylesheets -->
+  <link href="css/style.css" rel="stylesheet">
+  
+  <!--Favicon-->
+  <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+</head>
+<body>
+  <div class="page-wrapper">           
+<!--Page Title-->
+<section class="page-title text-center" style="background-image:url(images/about/doctor.PNG);" height="800vh;">
+    <div class="container">
+        <div class="title-text">
+            <h1>about us</h1>
+            <ul class="title-menu clearfix">
+                <li>
+                    <a href="index.html">home &nbsp;/</a>
+                </li>
+                <li>about us</li>
+            </ul>
+        </div>
+    </div>
+</section>
+<!--End Page Title-->
+
+<!-- Our Story -->
+<section class="story">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <img src="https://plus.unsplash.com/premium_photo-1673958772332-ca01085725f8?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="responsive" alt="story">
+            </div>
+            <div class="col-md-6">
+                <div class="story-content">
+                    <h2>Our Story</h2>
+              
+                    <p>Our departments work diligently to ensure the best possible care for our patients, offering expertise and compassion to address their unique medical needs and promote their health and well-being.</p>
+                    <h6>Mission</h6>
+                    <p>At YouCare, our mission is to restore health through compassionate, cutting‑edge care, delivered with dignity and respect.</p>
+                    <h6>Vision</h6>
+                    <p>We envision a community where every patient feels seen, heard, and cared for. Our values—Empathy, Excellence, Holistic Healing, Innovation, and Community Connection—guide every moment of every day.</p>
+                    <h6>What We Offer<h6>
+                    <p>A suite of services founded on expertise and empathy:<br><br>
+                       * Emergency, Inpatient & Outpatient Care <br><br>
+                       *Specialized centers: Cardiology, Oncology, Maternity, Pediatrics <br><br>
+                       *Community wellness initiatives & Virtual Health Programs. <br> <br>
+      </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+</section>
+<section class="gallery bg-gray">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="section-title text-center">
+                    <h3>Collected Shots
+                        <span>of Our Hospital</span>
+                    </h3>
+                    <p>Leverage agile frameworks to provide a robust synopsis for high level overv-
+                        <br>iews. Iterative approaches to corporate strategy...</p>
+                
+<div class="image-grid">
+  <img src="https://plus.unsplash.com/premium_photo-1661506746999-fff3a24b48e4?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDI4fHx8ZW58MHx8fHx8" alt="Image 1">
+  <img src="https://plus.unsplash.com/premium_photo-1661698586549-00580fb09a6b?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDczfHx8ZW58MHx8fHx8" alt="Image 2">
+  <img src="https://plus.unsplash.com/premium_photo-1661779739047-c5c27cf8ebac?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDUwfHx8ZW58MHx8fHx8" alt="Image 3">
+  <img src="https://plus.unsplash.com/premium_photo-1661600513061-80f4a662375d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image 4">
+  <img src="https://plus.unsplash.com/premium_photo-1661729571651-bbf946a93147?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDY2fHx8ZW58MHx8fHx8" alt="Image 5">
+  <img src="https://plus.unsplash.com/premium_photo-1661458147252-85d21d903fe3?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Image 6">
+</div>
+ </div>
+            </div>
+<hr>
+<!-- Contact Section -->
+<section class="appoinment-section section">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="accordion-section">
+    <div class="section-title">
+        <h3>FAQ</h3>
+    </div>
+    <div class="accordion-holder">
+        <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingOne">
+                    <h4 class="panel-title">
+                        <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Why Should I choose Medical Health
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                    <div class="panel-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                        non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh
+                        helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher
+                        vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't
+                        heard of them accusamus labore sustainable VHS.
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingTwo">
+                    <h4 class="panel-title">
+                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseTwo" aria-expanded="false"
+                            aria-controls="collapseTwo">
+                            What are the Centre’s visiting hours?
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+                    <div class="panel-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                        non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh
+                        helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher
+                        vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't
+                        heard of them accusamus labore sustainable VHS.
+                    </div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading" role="tab" id="headingThree">
+                    <h4 class="panel-title">
+                        <a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#collapseThree" aria-expanded="false"
+                            aria-controls="collapseThree">
+                            How many visitors are allowed?
+                        </a>
+                    </h4>
+                </div>
+                <div id="collapseThree" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingThree">
+                    <div class="panel-body">
+                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute,
+                        non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor,
+                        sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh
+                        helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher
+                        vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't
+                        heard of them accusamus labore sustainable VHS.
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+            </div>
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="contact-area">
+    <div class="section-title">
+        <h3>Request
+            <span>Appointment</span>
+        </h3>
+    </div>
+    <form name="contact_form" class="default-form contact-form" action="sendmail.php" method="post">
+        <div class="row">
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="form-group">
+                    <input type="text" name="Name" placeholder="Name" required="">
+                </div>
+                <div class="form-group">
+                    <input type="email" name="Email" placeholder="Email" required="">
+                </div>
+                <div class="form-group">
+                    <select name="subject">
+                        <option>Departments</option>
+                        <option>Diagnostic</option>
+                        <option>Psychological</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-6 col-sm-12 col-xs-12">
+                <div class="form-group">
+                    <input type="text" name="Phone" placeholder="Phone" required="">
+                </div>
+                <div class="form-group">
+                    <input type="text" name="Date" placeholder="Date" required="" id="datepicker">
+                    <i class="fa fa-calendar" aria-hidden="true"></i>
+                </div>
+                <div class="form-group">
+                    <select name="subject">
+                        <option>Doctor</option>
+                        <option>Diagnostic</option>
+                        <option>Psychological</option>
+                    </select>
+                </div>
+            </div>
+            <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="form-group">
+                    <textarea name="form_message" placeholder="Your Message" required=""></textarea>
+                </div>
+                <div class="form-group text-center">
+                    <button type="submit" class="btn-style-one">submit now</button>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Contact Section -->
+</div>
+<!--End pagewrapper-->
+
+
+<!--Scroll to top-->
+<div class="scroll-to-top scroll-to-target" data-target=".header-top">
+  <span class="icon fa fa-angle-up"></span>
+</div>
+
+<script src="plugins/jquery.js"></script>
+<script src="plugins/bootstrap.min.js"></script>
+<script src="plugins/bootstrap-select.min.js"></script>
+<!-- Slick Slider -->
+<script src="plugins/slick/slick.min.js"></script>
+<!-- FancyBox -->
+<script src="plugins/fancybox/jquery.fancybox.min.js"></script>
+<!-- Google Map -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCC72vZw-6tGqFyRhhg5CkF2fqfILn2Tsw"></script>
+<script src="plugins/google-map/gmap.js"></script>
+
+<script src="plugins/validate.js"></script>
+<script src="plugins/wow.js"></script>
+<script src="plugins/jquery-ui.js"></script>
+<script src="plugins/timePicker.js"></script>
+<script src="js/script.js"></script>
+<?php include "footer.php" ?>
+
+</body>
+
+</html>
